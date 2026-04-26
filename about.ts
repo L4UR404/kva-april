4d633fb199f122fb3299229d45bdd6a616fe469c
LAURA <laura.jankovic.23@singimail.rs>
2026-04-26 12:35:54 +0200

    Add files via upload

---
+++ about.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import axios from 'axios';
+import {ToyModel} from '../../models/toy.model';
+@Component({
+  selector: 'app-about',
+  imports: [],
+  templateUrl: './about.html',
+  styleUrl: './about.css',
+})
+
+export class About {
+ 
+}
